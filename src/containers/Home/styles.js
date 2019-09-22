bf7b21fafef94e0de0a2dd3d3ac3c4fb7ca89bd4
Amaru Ugaz <amaru.ugaz@peanuthub.cl>
2019-09-22 14:58:46 -0300

    import project

---
+++ src/containers/Home/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f76067'
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: '800',
+    color: 'white'
+  }
+});
+
+export default styles;
---
+++ src/navigators/HomeNavigator.js
@@ -0,0 +1,14 @@
+import { createStackNavigator } from 'react-navigation';
+
+import Home from '../containers/Home';
+
+const HomeNavigator = createStackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      header: null
+    }
+  }
+});
+
+export default HomeNavigator;
